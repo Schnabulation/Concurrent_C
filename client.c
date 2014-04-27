8598be469183f2364c22fca24a8e4c689540189f
Steve <steve@UbuntuVM.(none)>
2014-04-27 16:11:50 +0200

    Implemented basic server functions and created a small client for testing

---
+++ client.c
@@ -0,0 +1,60 @@
+/* Author: Steve Heller
+ * 
+ * Small client for testing purposes.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include "editorFunctions.h"
+
+void main() {
+  printf("Starting Client!\n");
+  printf("Please wait...\n");
+
+  int sock;
+  struct sockaddr_in serverAddress;
+  unsigned short server_port;
+  char *server_ip;
+  int bytes_received, total_bytes_received;
+
+  server_ip = "127.0.0.1";
+  server_port = 2354;
+  
+  memset(&serverAddress, 0, sizeof(serverAddress));
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_addr.s_addr = inet_addr(server_ip);
+  serverAddress.sin_port = server_port;
+
+  int breakUp = 0;
+  while (breakUp == 0) {
+    char input_string[256];
+    char *p;
+    unsigned int inputStringLen;
+    fgets(input_string, sizeof(input_string), stdin);       
+    if ((p = strchr(input_string, '\n')) != NULL) {
+      *p = '\0';
+    }
+    if (strcmp(input_string,"EXIT") == 0) {
+      printf("You disconnected!\n");
+      breakUp = 1;
+    }
+    inputStringLen = strlen(input_string);
+    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    connect(sock, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
+    int count = send(sock, input_string, inputStringLen, 0);
+    if (count != inputStringLen) {
+      error("send() sent a different number of bytes than expected");
+    }
+    close(sock);
+  }
+
+}
--- editor.c
+++ editor.c
@@ -17,10 +17,59 @@
 #include <netinet/tcp.h>
 #include "editorFunctions.h"
 
+void runCommand(char inputArg[]) {
+  if (strcmp(inputArg,"InsertLines") == 0) {
+    InsertLines();
+  } else if (strcmp(inputArg,"ReplceLines") == 0) {
+    ReplaceLines();
+  } else if (strcmp(inputArg,"ReadLines") == 0) {
+    ReadLines();
+  } else if (strcmp(inputArg,"DeleteLines") == 0) {
+    DeleteLines();
+  } else if (strcmp(inputArg,"NumLines") == 0) {
+    NumLines();
+  } else {
+    printf("Invalid command\n");
+  }
+}
+
 void startServer() {
-  printf("Server STARTED!\n");
-  printf("Running NumLines now\n");
-  NumLines();
+  printf("Starting Server!\n");
+  printf("Please hold on...\n");
+
+  char buffer[256];
+
+  int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+  struct sockaddr_in serverAddress;
+  struct sockaddr_in clientAddress;
+  unsigned int clientAddressLen;
+
+  memset(&serverAddress, 0, sizeof(serverAddress));
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+  serverAddress.sin_port = 2354;
+
+  if (bind(sock, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) < 0) {
+     error("ERROR on binding");
+  }
+  listen(sock, 5);
+  printf("Server started - waiting for connection on Port %i\n",serverAddress.sin_port);
+
+  int breakUp = 0;
+  while (breakUp == 0) {
+    int clientSock = accept(sock, (struct sockaddr *) & clientAddress, &clientAddressLen);
+    bzero(buffer,256);  
+    read(clientSock,buffer,255);
+    if (strcmp(buffer,"EXIT") == 0) {
+      printf("Client disconnected\n");
+      breakUp = 1;
+    } else {
+      runCommand(buffer);
+    }
+    close(clientSock);
+  }
+  close(sock);
 }
 
 void main(int argc, char *argv[]) {
